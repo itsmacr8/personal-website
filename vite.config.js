6d3382a03d9b098a5353ce2cb94eb594e40f8b50
ITS MAC <asifchowdhuryrafi143@outlook.com>
2023-11-30 22:39:04 +0600

    Add vite configuration for multi-page app.

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                work: resolve(__dirname, 'work.html'),
+                contact: resolve(__dirname, 'contact.html'),
+            },
+        },
+    },
+});
