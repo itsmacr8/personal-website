45d34969ddb0bb9efa29b3df5cfa1e34e66e1c4c
ITS MAC <asifchowdhuryrafi143@outlook.com>
2023-12-27 22:03:32 +0600

    Improve navigation and add theme toggle icon.
    -Replaces the custom navigation with the standard one.
    -Checks the user’s default theme settings and applies them automatically.
    -Adds an icon that allows the user to switch between dark and light themes.

--- src/ts/components/_menu.ts
+++ src/ts/components/_menu.ts
@@ -1,14 +1,69 @@
 // Select DOM Items
-const menuBtn = document.querySelector('.menu-btn')!;
-const menu = document.querySelector('.menu')!;
-const menuNav = document.querySelector('.menu__nav')!;
-const menuBranding = document.querySelector('.menu__branding')!;
-const navItems = document.querySelectorAll('.menu__nav__item')!;
+const body = document.querySelector('body')! as HTMLBodyElement,
+    menu = document.querySelector('.menu')! as HTMLDivElement,
+    UIIcon = document.querySelector('.ui-icon')! as HTMLDivElement,
+    brandLogo = document.querySelector('.brand__logo')! as HTMLAnchorElement,
+    sunIcon = document.querySelector('.ui-icon__sun')! as HTMLElement,
+    moonIcon = document.querySelector('.ui-icon__moon')! as HTMLElement,
+    menuOpen = document.querySelector('.hamburger-icon')! as HTMLElement,
+    menuClose = document.querySelector('.menu__close')! as HTMLElement;
 
-menuBtn.addEventListener('click', () => {
-    menuBtn.classList.toggle('close');
-    menu.classList.toggle('show');
-    menuNav.classList.toggle('show');
-    menuBranding.classList.toggle('show');
-    navItems.forEach((item) => item.classList.toggle('show'));
+
+function getUIMode() {
+    return localStorage.getItem('ui-mode');
+}
+
+// If the user has dark mode enabled or
+// he has previously visited with dark mode enabled
+function InitUIMode() {
+    const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const UIMode = getUIMode();
+    if (UIMode !== 'light' && isDark || UIMode === 'dark') {
+        body.classList.add('dark');
+        sunIcon.classList.add('active-ui');
+    } else if (!isDark || UIMode === 'light') {
+        moonIcon.classList.add('active-ui');
+    }
+}
+
+// Save UI mode in localStorage
+function UpdateUIMode() {
+    if (body.classList.contains('dark')) {
+        localStorage.setItem('ui-mode', 'dark');
+    } else {
+        localStorage.setItem('ui-mode', 'light');
+    }
+}
+
+// Update the UI icon
+function updateIcon() {
+    const currentMode = getUIMode()
+    if (currentMode === 'light') {
+        moonIcon.classList.add('active-ui');
+        sunIcon.classList.remove('active-ui');
+    } else if (currentMode === 'dark') {
+        sunIcon.classList.add('active-ui');
+        moonIcon.classList.remove('active-ui');
+    }
+}
+
+// Toggle UI mode
+UIIcon.addEventListener('click', () => {
+    body.classList.toggle('dark');
+    UpdateUIMode()
+    updateIcon()
 });
+
+//  Open mobile menu
+menuOpen.addEventListener('click', () => {
+    menu.classList.add('menu--active-js');
+    brandLogo.classList.add('brand__logo-js');
+});
+
+//  Close mobile menu
+menuClose.addEventListener('click', () => {
+    menu.classList.remove('menu--active-js');
+    brandLogo.classList.remove('brand__logo-js');
+});
+
+InitUIMode()
