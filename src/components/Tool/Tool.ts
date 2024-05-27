0c13a2cbbb8784fbba1dbce3d407cad3e1c63995
ITS MAC <asifchowdhuryrafi143@outlook.com>
2024-05-28 00:28:06 +0600

    Refactor: Use the card markup to render tool
    Remove the tool markup and use the same card markup to render the tool for reusability.

--- src/components/Tool/Tool.ts
+++ src/components/Tool/Tool.ts
@@ -1,9 +1,8 @@
-import { showToolMarkup } from "./_tool_markup";
 import { toolsTable } from "../_variables";
-import { getDatabaseRecords, renderDatabaseRecords } from '../_utils'
+import { getDatabaseRecords, renderDatabaseRecords, cardMarkup } from '../_utils'
 
 renderDatabaseRecords(
   await getDatabaseRecords(toolsTable),
   document.querySelector('.tools') as HTMLDivElement,
-  showToolMarkup
+  cardMarkup
 );
--- src/components/_utils.ts
+++ src/components/_utils.ts
@@ -67,7 +67,8 @@ function renderDatabaseRecords(
 }
 
 function cardMarkup(card: DatabaseRecord, index: number) {
-  return `<div class="card" id="card-${index}">
+  const cardName = card.ButtonText === 'Read More' ? "article" : "tool";
+  return `<div class="card" id="${cardName}-${index}">
     <div><img class="card__thumbnail" src="${card.Thumbnail}" alt="${card.Title} thumbnail" title="${card.Title} thumbnail"></div>
     <div class="card__body">
       <h3 class="card__title">${card.Title}</h3>
