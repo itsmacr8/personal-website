c168ec5585e48c185bede72dab15de2f5e04ea46
ITS MAC <asifchowdhuryrafi143@outlook.com>
2024-07-06 18:24:54 +0600

    feat: fetch more projects from database
    See more projects from the homepage rather than going to a new page.

--- src/components/_variables.ts
+++ src/components/_variables.ts
@@ -7,6 +7,7 @@ const projectDescriptionTable: string = import.meta.env.VITE_AIRTABLE_PW_PDTN;
 const toolsTable: string = import.meta.env.VITE_AIRTABLE_PW_TTN;
 const experienceTable: string = import.meta.env.VITE_AIRTABLE_PW_ETN;
 const articleTable: string = import.meta.env.VITE_AIRTABLE_PW_ATN;
+const fetchProjectNumber: number = Number(import.meta.env.VITE_FETCH_PROJECT_NUMBER);
 const loader = document.getElementById('loader-container') as HTMLDivElement
 
 export {
@@ -20,4 +21,5 @@ export {
   experienceTable,
   articleTable,
   loader,
+  fetchProjectNumber
 };
--- src/components/project/Project.ts
+++ src/components/project/Project.ts
@@ -2,9 +2,26 @@ import './Project.scss'
 import { showProjectsMarkup } from "./_project_markup";
 import { projectDescriptionTable } from '../_variables';
 import { getDatabaseRecords, renderDatabaseRecords } from '../_utils'
+import { addClassTo, removeClassFrom } from '../_utils';
+import { loader, fetchProjectNumber } from '../_variables';
+
+const projectContainer = document.querySelector(".projects") as HTMLDivElement
+const loadMoreProjects = document.getElementById('load-more-projects') as HTMLButtonElement;
+
 
 renderDatabaseRecords(
   await getDatabaseRecords(projectDescriptionTable),
-  document.querySelector(".projects") as HTMLDivElement,
+  projectContainer,
   showProjectsMarkup
 );
+
+loadMoreProjects.addEventListener('click', async () => {
+  removeClassFrom(loader);
+  const projects = await getDatabaseRecords(projectDescriptionTable, fetchProjectNumber);
+  renderDatabaseRecords(
+      projects.slice(3),
+      projectContainer,
+      showProjectsMarkup
+    );
+  addClassTo(loader);
+})
