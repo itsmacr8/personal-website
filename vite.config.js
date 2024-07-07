459e535d22a68ac00a052148351eb78bf85c9104
ITS MAC <asifchowdhuryrafi143@outlook.com>
2024-07-07 18:37:47 +0600

    Refactor: remove work page
    Remove the work page that used to show projects and its associate images

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,13 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                work: resolve(__dirname, 'work.html'),
-                interests: resolve(__dirname, 'interests.html'),
-            },
-        },
-        target: 'ES2022'
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        interests: resolve(__dirname, 'interests.html'),
+      },
     },
+    target: 'ES2022'
+  },
 });
