4399fca1da78dc713e3acac184fba3498a7dddb9
ITS MAC <asifchowdhuryrafi143@outlook.com>
2024-07-13 12:47:28 +0600

    Feat: Create pagination component

---
+++ src/components/Pagination/Pagination.ts
@@ -0,0 +1,76 @@
+import './Pagination.scss';
+
+class Pagination {
+  totalPages: number;
+  currentPage = 1;
+  pagination: HTMLDivElement = this.getPaginationWrapper();
+
+  constructor(totalPages: number) {
+    this.totalPages = totalPages;
+  }
+
+  getPaginationWrapper() {
+    return document.getElementById('pagination') as HTMLDivElement;
+  }
+
+  createPaginationWrapper(element: HTMLDivElement) {
+    if (this.getPaginationWrapper()) return;
+    element.insertAdjacentHTML(
+      'afterend',
+      `<div class="pagination" id="pagination"></div>`
+    );
+    this.pagination = this.getPaginationWrapper();
+  }
+
+  show(currentPage: number = 1) {
+    this.pagination.innerHTML = '';
+    this.currentPage = currentPage;
+    const { start, end } = this.getCurrentNumber();
+    this.prevButton();
+    for (let btn = start; btn <= end; btn++) this.buttons(btn);
+    this.nextButton();
+    this.updatePagination();
+  }
+
+  getCurrentNumber() {
+    let start = Math.max(1, this.currentPage - 2);
+    const end = Math.min(this.totalPages, start + 4);
+    start = Math.max(1, end - 4);
+    return { start, end };
+  }
+
+  prevButton() {
+    this.currentPage > 1 && this.button('Prev', this.currentPage - 1);
+  }
+
+  nextButton() {
+    const currentPage = this.currentPage;
+    currentPage < this.totalPages && this.button('Next', currentPage + 1);
+  }
+
+  button(text: string, value: number) {
+    this.pagination.insertAdjacentHTML(
+      'beforeend',
+      `<button class="btn" data-pagination='${value}'>${text}</button>`
+    );
+  }
+
+  buttons(number: number) {
+    this.pagination.insertAdjacentHTML(
+      'beforeend',
+      `<button class="btn btn--pagination" data-pagination="${number}"
+      ${number === this.currentPage ? 'disabled' : ''}>${number}</button>`
+    );
+  }
+
+  updatePagination() {
+    this.pagination.addEventListener('click', (event) => {
+      const target = event.target as HTMLElement;
+      if (target.tagName === 'BUTTON') {
+        this.show(Number(target.dataset.pagination));
+      }
+    });
+  }
+}
+
+export { Pagination };
