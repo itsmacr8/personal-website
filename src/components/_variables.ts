ae4d455c9dd737ac721039b17a02561ef7036d43
ITS MAC <asifchowdhuryrafi143@outlook.com>
2024-09-10 23:08:29 +0600

    Remove tools section

--- src/components/_variables.ts
+++ src/components/_variables.ts
@@ -7,7 +7,6 @@ const aboutTextRecord: string = import.meta.env.VITE_PW_ATR;
 const projectDescriptionTable: string = import.meta.env.VITE_PW_PDTN;
 const testimonial: string = import.meta.env.VITE_PW_TN;
 const testimonialRecordID: string = import.meta.env.VITE_PW_TR;
-const toolsTable: string = import.meta.env.VITE_PW_TTN;
 const experienceTable: string = import.meta.env.VITE_PW_ETN;
 const articleTable: string = import.meta.env.VITE_PW_ATN;
 const totalProject: number = Number(import.meta.env.VITE_TOTAL_PROJECT);
@@ -23,7 +22,6 @@ export {
   projectDescriptionTable,
   testimonial,
   testimonialRecordID,
-  toolsTable,
   experienceTable,
   articleTable,
   loader,
--- src/components/utilities/LazyLoading.ts
+++ src/components/utilities/LazyLoading.ts
@@ -4,7 +4,6 @@ import { showProjectsMarkup } from '../project/_project_markup';
 import {
   AirTableDB,
   experienceTable,
-  toolsTable,
   articleTable,
   projectDescriptionTable,
   projectsContainer,
@@ -12,23 +11,18 @@ import {
 import { initializeSlider } from '../Slider/Slider';
 
 const xSec = document.getElementById('experience-section') as HTMLElement;
-const toolsSec = document.getElementById('tools-section') as HTMLElement;
 const projectsSec = document.getElementById('portfolio-section') as HTMLElement;
 const articlesSec = document.getElementById('articles-section') as HTMLElement;
 const testimonialsSec = document.getElementById('testimonials') as HTMLElement;
 const xContainer = document.querySelector('.experiences') as HTMLDivElement;
-const toolsContainer = document.querySelector('.tools') as HTMLDivElement;
 const articlesContainer = document.querySelector('.articles') as HTMLDivElement;
 const xData = await AirTableDB.getRecords(experienceTable);
-const toolsData = await AirTableDB.getRecords(toolsTable);
 const projectsData = await AirTableDB.getRecords(projectDescriptionTable);
 const articlesData = await AirTableDB.getRecords(articleTable);
 
 function observeSection(currentSec: Element) {
   if (currentSec === xSec)
     renderDatabaseRecords(xData, xContainer, showExperiencesMarkup);
-  else if (currentSec === toolsSec)
-    renderDatabaseRecords(toolsData, toolsContainer, cardMarkup);
   else if (currentSec === projectsSec)
     renderDatabaseRecords(projectsData, projectsContainer, showProjectsMarkup);
   else if (currentSec === articlesSec)
@@ -53,7 +47,6 @@ const observer = new IntersectionObserver((entries, observer) => {
 }, options);
 
 observer.observe(xSec);
-observer.observe(toolsSec);
 observer.observe(projectsSec);
 observer.observe(articlesSec);
 observer.observe(testimonialsSec);
