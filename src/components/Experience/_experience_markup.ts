7d2fe34b0ca418c50ace87ca786bed7a6a108c2b
ITS MAC <asifchowdhuryrafi143@outlook.com>
2024-11-15 18:01:49 +0600

    refactor: replace double quotes with singles for consistency

--- src/components/Experience/_experience_markup.ts
+++ src/components/Experience/_experience_markup.ts
@@ -1,11 +1,11 @@
-import { DatabaseRecord } from "../../types/DatabaseRecord.interface"
+import { DatabaseRecord } from '../../types/DatabaseRecord.interface'
 
 function showExperiencesMarkup(experience:DatabaseRecord, index:number) {
-  return`<div class="${experience.Order}" id="experience-${index+1}">
-    <h3 class="experience__position">${experience.Position}</h3>
-    <h4 class="experience__company">${experience.Company}</h4>
-    <p class="experience__period">${experience.Duration}</p>
-    <ul class="experience__contributions">${experience.Contributions}</ul>
+  return`<div class='${experience.Order}' id='experience-${index+1}'>
+    <h3 class='experience__position'>${experience.Position}</h3>
+    <h4 class='experience__company'>${experience.Company}</h4>
+    <p class='experience__period'>${experience.Duration}</p>
+    <ul class='experience__contributions'>${experience.Contributions}</ul>
   </div>`
 }
 
