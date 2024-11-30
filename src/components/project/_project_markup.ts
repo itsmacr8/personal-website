56d2ddbd8c8a0a0596050772f6ef3147cbd81a0d
ITS MAC <asifchowdhuryrafi143@outlook.com>
2024-11-30 17:21:27 +0600

    refactor: code formatting for better readability

--- src/components/project/_project_markup.ts
+++ src/components/project/_project_markup.ts
@@ -1,11 +1,11 @@
 import { createArray, listTags } from '../_utils';
 import { DatabaseRecord } from '../../types/DatabaseRecord.interface';
 
-function showProjectsMarkup(project:DatabaseRecord, index:number) {
-  const tagListArray =
-    typeof project.TagList === 'string' ? createArray(project.TagList) : [];
+function showProjectsMarkup(project: DatabaseRecord, index: number) {
+  const { TagList, AltText } = project;
+  const tagListArray = typeof TagList === 'string' ? createArray(TagList) : [];
 
-  return`<div class='project' id='project-${index+1}'>
+  return `<div class='project' id='project-${index + 1}'>
     <div class='${project.Order}'>
       <h3 class='project__name'>${project.Name}</h3>
       <p class='project__description'>${project.Description}</p>
@@ -13,20 +13,18 @@ function showProjectsMarkup(project:DatabaseRecord, index:number) {
         ${listTags(tagListArray)}
       </ul>
       <div class='project__cta-btn'>
-        <a href='${project.LiveView}'
-        class='btn'>
-        <i class='fas fa-eye'></i> Live View
+        <a href='${project.LiveView}' class='btn'>
+          <i class='fas fa-eye'></i> Live View
         </a>
-        <a href='${project.SourceCode}'
-        class='btn btn--outline'>
-        <i class='fas fa-code'></i> Source Code
+        <a href='${project.SourceCode}' class='btn btn--outline'>
+          <i class='fas fa-code'></i> Source Code
         </a>
       </div>
     </div>
     <div class='project__thumbnail'>
-      <img src='${project.Thumbnail}' title='${project.Name} thumbnail' alt='${project.Name} thumbnail'>
+      <img src='${project.Thumbnail}' alt='${AltText}' title='${AltText}'>
     </div>
-</div>`
+</div>`;
 }
 
-export { showProjectsMarkup }
+export { showProjectsMarkup };
