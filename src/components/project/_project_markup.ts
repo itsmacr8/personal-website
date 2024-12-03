eae150ce9f67816121b9fbe24fadef7f568ac247
ITS MAC <asifchowdhuryrafi143@outlook.com>
2024-12-03 19:34:10 +0600

    refactor: destructure all values for consistency

--- src/components/project/_project_markup.ts
+++ src/components/project/_project_markup.ts
@@ -2,27 +2,36 @@ import { createArray, listTags } from '../_utils';
 import { DatabaseRecord } from '../../types/DatabaseRecord.interface';
 
 function showProjectsMarkup(project: DatabaseRecord, index: number) {
-  const { TagList, AltText } = project;
+  const {
+    TagList,
+    Order,
+    Name,
+    Description,
+    LiveView,
+    SourceCode,
+    Thumbnail,
+    AltText,
+  } = project;
   const tagListArray = typeof TagList === 'string' ? createArray(TagList) : [];
 
   return `<div class='project' id='project-${index + 1}'>
-    <div class='${project.Order}'>
-      <h3 class='project__name'>${project.Name}</h3>
-      <p class='project__description'>${project.Description}</p>
+    <div class='${Order}'>
+      <h3 class='project__name'>${Name}</h3>
+      <p class='project__description'>${Description}</p>
       <ul class='project__tag-list'>
         ${listTags(tagListArray)}
       </ul>
       <div class='project__cta-btn'>
-        <a href='${project.LiveView}' class='btn'>
+        <a href='${LiveView}' class='btn'>
           <i class='fas fa-eye'></i> Live View
         </a>
-        <a href='${project.SourceCode}' class='btn btn--outline'>
+        <a href='${SourceCode}' class='btn btn--outline'>
           <i class='fas fa-code'></i> Source Code
         </a>
       </div>
     </div>
     <div class='project__thumbnail'>
-      <img src='${project.Thumbnail}' alt='${AltText}' title='${AltText}'>
+      <img src='${Thumbnail}' alt='${AltText}' title='${AltText}'>
     </div>
 </div>`;
 }
